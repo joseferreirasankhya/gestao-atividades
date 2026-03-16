124a6dce36633dcbac8b6fe95e35f1847db587f4
joseferreirasankhya <jose.ferreira@sankhya.com.br>
2026-03-16 13:12:50 -0300

    chore: ajustes dos KPIs de atividades

--- components/atividades/kpis/queries/outras_ativ.sql
+++ components/atividades/kpis/queries/outras_ativ.sql
@@ -1,42 +1,31 @@
+-- Cálculo de Outras Atividades Concluídas no Prazo (Lógica Corrigida)
+WITH ATIVIDADES_FILTRADAS AS (
+  SELECT
+    ATIV.DATA_CONCLUSAO,
+    ATIV.PREVISAO_CONCLUSAO
+  FROM CAD_ATIVIDADES ATIV
+  JOIN CAD_PARCEIRO PAR ON ATIV.ID_CLIENTE = PAR.ID
+  WHERE
+    (ATIV.ID_PLAYBOOKS = 5 OR ATIV.ID_PLAYBOOKS = -999) -- Condição corrigida para buscar pelo ID do playbook na atividade
+    AND (
+      DATE_FORMAT(ATIV.DATA_INICIO, '%Y%m%d') IN (:ID_CALENDARIO) OR 
+      'Todos' IN (:ID_CALENDARIO)
+    ) AND (
+      PAR.ID_UNIDADE_DO_PARCEIRO_ATUAL IN (:ID_UNIDADE) OR 
+      'Todos' IN (:ID_UNIDADE)
+    ) AND (
+      ATIV.ID_RESPONSAVEL IN (:ID_USUARIO) OR
+      'Todos' IN (:ID_USUARIO)
+    )
+)
 SELECT
-    'OUTRAS ATIVIDADES' AS TITULO,
-    'CONCLUSÃO NO PRAZO:' AS SUBTITULO,
-    (
-        SELECT COUNT(*)
-        FROM (
-            SELECT PB.ID
-            FROM CAD_PLAYBOOKS PB
-            JOIN CAD_ATIVIDADES ATIV ON PB.ID = ATIV.ID_PLAYBOOKS
-            JOIN CAD_PARCEIRO PAR ON ATIV.ID_CLIENTE = PAR.ID
-            WHERE PB.ID_TIPO_DE_PLAYBOOK IN (5, -999) AND (
-                DATE_FORMAT (ATIV.DATA_INICIO, '%Y%m%d') IN (:ID_CALENDARIO)
-                OR 'Todos' IN (:ID_CALENDARIO)
-            ) AND (
-                PAR.ID_UNIDADE_DO_PARCEIRO_ATUAL IN (:ID_UNIDADE)
-                OR 'Todos' IN (:ID_UNIDADE)
-            )
-            GROUP BY PB.ID
-            HAVING SUM(
-                CASE
-                    -- CORREÇÃO: Marcar com '1' se a atividade não foi concluída OU foi concluída com atraso.
-                    WHEN ATIV.DATA_CONCLUSAO IS NULL OR ATIV.DATA_CONCLUSAO > ATIV.PREVISAO_CONCLUSAO THEN 1
-                    ELSE 0
-                END
-            ) = 0
-        ) AS PlaybooksConcluidosNoPrazo
-    ) AS REALIZADO,
-    (
-        -- Adicionado DISTINCT para contar playbooks únicos
-        SELECT COUNT(DISTINCT PB.ID)
-        FROM CAD_PLAYBOOKS PB
-        JOIN CAD_ATIVIDADES ATIV ON PB.ID = ATIV.ID_PLAYBOOKS
-        JOIN CAD_PARCEIRO PAR ON ATIV.ID_CLIENTE = PAR.ID
-        WHERE PB.ID_TIPO_DE_PLAYBOOK IN (5, -999) AND (
-            DATE_FORMAT (ATIV.DATA_INICIO, '%Y%m%d') IN (:ID_CALENDARIO)
-            OR 'Todos' IN (:ID_CALENDARIO)
-        ) AND (
-            PAR.ID_UNIDADE_DO_PARCEIRO_ATUAL IN (:ID_UNIDADE)
-            OR 'Todos' IN (:ID_UNIDADE)
-        )
-    ) AS PREVISTO
-FROM DUAL
+  'OUTRAS ATIVIDADES' AS TITULO,
+  'CONCLUSÃO NO PRAZO:' AS SUBTITULO,
+  (
+    SELECT COUNT(*)
+    FROM ATIVIDADES_FILTRADAS
+    WHERE DATA_CONCLUSAO IS NOT NULL AND DATA_CONCLUSAO <= PREVISAO_CONCLUSAO
+  ) AS REALIZADO,
+  (
+    SELECT COUNT(*) FROM ATIVIDADES_FILTRADAS
+  ) AS PREVISTO;
